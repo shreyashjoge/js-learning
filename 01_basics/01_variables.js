70339a9477103753b286454339132c23a982b5d1
shreyashjoge <153541753+shreyashjoge@users.noreply.github.com>
2025-02-14 10:53:24 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 250140
+let accountEmail = "warm@gmail.com"
+var accountPassword = "12345"
+accountCity = "wardha"
+let accountState
+
+// accountId = 2 // not allowed
+
+/*
+prefer not to used var
+because of issue in blobk scope & functional scope
+*/
+
+accountEmail = 'kito@google.com'
+accountPassword = '25416'
+accountCity = "nagpur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
